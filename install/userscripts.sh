c5b6ff22771dae026b05e263744154099ca770b2
pospi <pospi@spadgos.com>
2025-11-13 04:08:36 +1100

    use standard bindir

--- install/userscripts.sh
+++ install/userscripts.sh
@@ -5,4 +5,4 @@
 
 # Media downloader to watch & listen to content offline
 # Usage: download_media.sh [URL]
-ln -s ~/.config/niri/install/userscripts/download_media.sh ~/bin/
+ln -s ~/.config/niri/install/userscripts/download_media.sh ~/.local/bin/
