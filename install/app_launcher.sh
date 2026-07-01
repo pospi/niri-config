9c69101f9755cbcfc6d2f8cf0647f00f4c106c85
pospi <pospi@spadgos.com>
2026-07-01 16:16:26 +1000

    update uLauncher to latest

--- install/app_launcher.sh
+++ install/app_launcher.sh
@@ -12,8 +12,8 @@
 
 pushd "$TEMPDIR"
 
-  wget https://github.com/Ulauncher/Ulauncher/releases/download/v6.0.0-beta27/ulauncher_6.0.0.beta27_all.deb
-  sudo dpkg -i ulauncher_6.0.0.beta27_all.deb
+  wget https://github.com/Ulauncher/Ulauncher/releases/download/v6.0.0-beta31/ulauncher_6.0.0.beta31_all.deb
+  sudo dpkg -i ulauncher_6.0.0.beta31_all.deb
   sudo apt install -y # autoresolve any dependency issues
 
 popd
